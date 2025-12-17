3e49e310a7a82df9c591a05976696b6c441ed386
STORM <stormvtube@gmail.com>
2025-12-17 13:51:59 -0500

    Added "nation cards" to home page, Redid File Structure, added needed comments.

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// confirms that the JS file is loaded
+onload = function() {
+    console.log("JavaScript Loaded");
+}
+
+function LinkHomepage() {
+    window.location.href("index.html");
+}
+
+function LinkAbout(){
+    alert("not set up yet");
+    // window.location.href("about.html");
+}
+
+function LinkBluemap() {
+    window.open("https://map.wariumsmp.top:8100", "_blank");
+}
+
+function LinkDiscord() {
+    alert("not set up yet");
+    // window.open("https://discord.gg/invitecode", "_blank");
+}
